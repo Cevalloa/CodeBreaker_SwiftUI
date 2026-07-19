7cbef312ac9b0d9bceac5d9c569911d16b0181c7
Alex <Cevalloa@gmail.com>
2026-07-19 18:25:47 -0400

    Using for each to separate cell indicies

--- CodeBreaker_SwiftUI/ContentView.swift
+++ CodeBreaker_SwiftUI/ContentView.swift
@@ -10,15 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-            Text("Hello, world!")
-                .font(.largeTitle)
-                .foregroundStyle(.orange)
-                .background(.yellow)
-                .padding()
-            Circle()
+            pegs(color: [.red, .green, .green, .yellow])
+            pegs(color: [.red, .blue, .green, .red])
+            pegs(color: [.red, .green, .green, .yellow])
         }.padding()
             .foregroundStyle(.blue)
+    }
+    
+    func pegs(color: Array<Color>) -> some View {
+        HStack {
+            ForEach(color.indices, id: \.self) { index in
+                Circle().foregroundStyle(color[index])
+            }
+        }
     }
 }
 
