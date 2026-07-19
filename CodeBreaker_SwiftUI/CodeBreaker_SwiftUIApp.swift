c11cc604ede8fb5d9ab967ae5347ddc6454a168f
Alex <Cevalloa@gmail.com>
2026-07-18 22:11:57 -0400

    Initial Commit

---
+++ CodeBreaker_SwiftUI/CodeBreaker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreaker_SwiftUIApp.swift
+//  CodeBreaker_SwiftUI
+//
+//  Created by Alex Cevallos on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreaker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreaker_SwiftUITests/CodeBreaker_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreaker_SwiftUITests.swift
+//  CodeBreaker_SwiftUITests
+//
+//  Created by Alex Cevallos on 7/18/26.
+//
+
+import Testing
+@testable import CodeBreaker_SwiftUI
+
+struct CodeBreaker_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
