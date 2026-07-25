5612a3e885637bbbd7b67feeac1dde10b3967ffb
Alex <Cevalloa@gmail.com>
2026-07-25 00:50:34 -0400

    Setting guess button overlay

--- CodeBreaker_SwiftUI/CodeBreakerView.swift
+++ CodeBreaker_SwiftUI/CodeBreakerView.swift
@@ -8,32 +8,34 @@
 import SwiftUI
 
 struct CodeBreakerView: View {
-    @State var game: CodeBreaker = CodeBreaker(pegChoices: [.brown, .yellow, .orange, .black])
-    
+    @State var game: CodeBreaker = CodeBreaker(pegChoices: [
+        .brown, .yellow, .orange, .black,
+    ])
+
     var body: some View {
         VStack {
             view(for: game.masterCode)
-//            pegs(color: [.red, .green, .green, .yellow])
+            //            pegs(color: [.red, .green, .green, .yellow])
             ScrollView {
                 view(for: game.guess)
-                ForEach(game.attempts.indices.reversed(), id:\.self) { index in
+                ForEach(game.attempts.indices.reversed(), id: \.self) { index in
                     view(for: game.attempts[index])
                 }
             }
         }
         .padding()
     }
-    
+
     var guessButton: some View {
         Button("Guess") {
-            withAnimation{
+            withAnimation {
                 game.attemptGuess()
             }
         }
         .font(.system(size: 80))
         .minimumScaleFactor(0.1)
     }
-    
+
     func view(for code: Code) -> some View {
         HStack {
             ForEach(code.pegs.indices, id: \.self) { index in
@@ -47,18 +49,26 @@ struct CodeBreakerView: View {
                     .contentShape(Rectangle())
                     .aspectRatio(1, contentMode: .fit)
                     .foregroundStyle(code.pegs[index]).onTapGesture {
-                        
+
                         if code.kind == .guess {
                             game.changeGuessPeg(at: index)
                         }
                     }
             }
-            MatchMarkers(matches: code.matches)
-                .overlay {
-                    if code.kind == .guess {
-                        guessButton
-                    }
+            Rectangle().foregroundStyle(Color.clear).aspectRatio(
+                1,
+                contentMode: .fit
+            )
+            .overlay {
+                if let matches = code.matches {
+                    MatchMarkers(matches: matches)
+                        .overlay {
+                            if code.kind == .guess {
+                                guessButton
+                            }
+                        }
                 }
+            }
         }
     }
 }
